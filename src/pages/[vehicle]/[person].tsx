3de89e22605faed62e304caaa573fe84f3cc1de7
mikohan <angara99@gmail.com>
2020-11-11 12:16:42 +0300

    Conditional loading

--- src/pages/[vehicle]/[person].tsx
+++ src/pages/[vehicle]/[person].tsx
@@ -1,13 +1,73 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+/* import { IVehicles } from '~/interfaces/vehicles'; */
 
-export default function Person() {
+export default function Person(props) {
   const router = useRouter();
-  const { vehicle, person } = router.query;
+  /* const { vehicle, person } = router.query; */
+  const [data, setData] = useState(props.data);
+  const { vehicle, person, details } = data;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch(
+        `http://localhost:3000/api/hello?person=${router.query.person}`
+      );
+      const d = await response.json();
+      setData(d);
+      setLoading(false);
+    }
+    if (Object.keys(props.data).length === 0) {
+      setLoading(true);
+      loadData();
+    }
+  }, []);
   return (
     <div>
-      <h1>
-        Person: {person} Has a car: {vehicle}
-      </h1>
+      {loading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          <h1>
+            Person: {person} Has a car: {vehicle}
+          </h1>
+          <p>{details}</p>
+        </div>
+      )}
     </div>
   );
 }
+
+Person.getInitialProps = async (ctx) => {
+  if (!ctx.req) {
+    return { data: {} };
+  }
+  const { query } = ctx;
+  const response = await fetch(
+    `http://localhost:3000/api/hello?person=${query.person}`
+  );
+  const data = await response.json();
+
+  return { data: data };
+};
+
+/* export const getStaticPaths: GetStaticPaths = async () => { */
+/*   const res = await fetch(`http://localhost:3000/api/hello`); */
+/*   const vehicles = await res.json(); */
+/*   const paths = vehicles.map((vehicle: IVehicles) => ({ */
+/*     params: { vehicle: vehicle.vehicle, person: vehicle.ownerName }, */
+/*   })); */
+/*   console.log(paths); */
+/*   return { paths, fallback: false }; */
+/* }; */
+
+/* export const getStaticProps: GetStaticProps = async (context) => { */
+/*   console.log(context); */
+/*   const response = await fetch(`http://localhost:3000/api/hello?id=${1}`); */
+/*   const owner = await response.json(); */
+/*   return { */
+/*     props: owner, */
+/*   }; */
+/* }; */
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,14 +2,18 @@
 import { vehicles } from '~/pages/api/data/vehicles';
 
 export default (req, res) => {
-  console.log(req.query);
+  // console.log(req.query);
   if (Object.keys(req.query).length !== 0) {
-    const id = req.query.id;
-    const v = vehicles.find((v) => v.id === +id);
-    res.statusCode = 200;
-    res.json(v);
+    setTimeout(() => {
+      const person = req.query.person;
+      const v = vehicles.find((v) => v.ownerName === person);
+      res.statusCode = 200;
+      res.json(v);
+    }, 3000);
   } else {
-    res.statusCode = 200;
-    res.json(vehicles);
+    setTimeout(() => {
+      res.statusCode = 200;
+      res.json(vehicles);
+    }, 3000);
   }
 };
