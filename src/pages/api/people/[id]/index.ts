95dd992e8ce17d6b2ded7a1072e6f557f29d5006
mikohan <angara99@gmail.com>
2020-11-11 17:15:02 +0300

    End for now

--- src/pages/api/people/[id]/index.ts
+++ src/pages/api/people/[id]/index.ts
@@ -6,6 +6,17 @@ export default async function getPerson(
   res: NextApiResponse
 ) {
   const db = await dbOpen();
+
+  if ((req.method = 'PUT')) {
+    const statement = await db.prepare(`UPDATE Person SET name = ?, email = ? WHERE
+                                       id = ?`);
+    const result = await statement.run(
+      req.body.name,
+      req.body.email,
+      req.query.id
+    );
+    await statement.finalize();
+  }
   const person = await db.get(`SELECT * FROM Person WHERE id = ?`, [
     req.query.id,
   ]);
