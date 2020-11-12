2c8550f203de61d3ed7d3e6cf2aca063eeb67a84
mikohan <angara99@gmail.com>
2020-11-12 18:46:06 +0300

    Added list

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -12,12 +12,35 @@ import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
 export default function HomePage() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm"></Container>
+      <Container maxWidth="sm">
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List component="nav" aria-label="secondary mailbox folders">
+          <ListItem button>
+            <ListItemText primary="Trash" />
+          </ListItem>
+        </List>
+      </Container>
     </React.Fragment>
   );
 }
