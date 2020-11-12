7bf2b7e0531bd1136e7e4de5e405b2637ce8f875
mikohan <angara99@gmail.com>
2020-11-12 18:35:30 +0300

    Added comment at theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,6 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
+// Added some comments
 const theme = createMuiTheme({
   palette: {
     primary: {
