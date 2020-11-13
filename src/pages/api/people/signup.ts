105d65d58e0802f44cc0328fa7a12994f49677d1
mikohan <angara99@gmail.com>
2020-11-13 14:30:04 +0300

    Signup working

--- src/pages/api/people/signup.ts
+++ src/pages/api/people/signup.ts
@@ -1,5 +1,6 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 import { dbOpen } from '~/pages/api/dbopen';
+import { hash } from 'bcrypt';
 
 export default async function getPerson(
   req: NextApiRequest,
@@ -8,18 +9,16 @@ export default async function getPerson(
   const db = await dbOpen();
 
   if (req.method === 'POST') {
-    const statement = await db.prepare(
-      `INSERT INTO Person (name, email, password) VALUES ( ?, ?, ? )`
-    );
-    const result = await statement.run(
-      req.body.name,
-      req.body.email,
-      req.body.password
-    );
-    await statement.finalize();
+    hash(req.body.password, 10, async function (err, hash) {
+      const statement = await db.prepare(
+        `INSERT INTO Person (name, email, password) VALUES ( ?, ?, ? )`
+      );
+      const result = await statement.run(req.body.name, req.body.email, hash);
+      await statement.finalize();
 
-    const person = await db.get(`SELECT * FROM Person `, [req.query.id]);
-    res.json(person);
+      const person = await db.get(`SELECT * FROM Person `, [req.query.id]);
+      res.json(person);
+    });
   } else {
     res.status(405).json({ message: `Only POST method is allowed` });
   }
