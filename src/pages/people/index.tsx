92815e5182d66dfdba6cee72e29064f4d9310c92
mikohan <angara99@gmail.com>
2020-11-14 19:33:29 +0300

    Page people make

--- src/pages/people/index.tsx
+++ src/pages/people/index.tsx
@@ -1,3 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse, NextPageContext } from 'next';
+import { __appUrl__ } from '~/config';
 
-import { dbOpen } from '~/pages/api/dbopen';
+export default function People(props: any) {
+  return (
+    <div>
+      <h1>Page People</h1>
+    </div>
+  );
+}
+
+People.getInitialProps = async function (context: NextPageContext) {
+  const response = await fetch(`${__appUrl__}/api/people`);
+  const json = await response.json();
+  return json;
+};
